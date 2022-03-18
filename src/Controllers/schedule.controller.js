6b41c95f0d6e5389dbbddc9f80f13ef186f25695
Dhruvkagrawal <dhruvkumaragrawal7@gmail.com>
2022-03-18 20:47:21 +0530

    created the basic post request app is working fine

--- src/Controllers/schedule.controller.js
+++ src/Controllers/schedule.controller.js
@@ -1,3 +1,13 @@
 const { Router } = require("express");
-
+const res = require("express/lib/response");
 const router = Router();
+
+const Scheules = require("../Models/schedule.model");
+
+router.post("", (req, res) => {
+  try {
+    return res.status(200).send("Post request runs succesfully");
+  } catch (error) {
+    return res.status(500).send({ status: "Failed", message: error.message });
+  }
+});
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const connect = require("./Config/db");
 
 app.use(express.json());
 
-const start = async () => {
+const start = async (PORT) => {
   await connect();
   app.listen(PORT, () => {
     console.log("Current Server is Running on PORT", PORT);
